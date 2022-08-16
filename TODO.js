3d47f1c782ad70e3e9c85e6b636f488cd131a1f5
“Ben <123bfran@gmail.com>
2022-08-16 13:14:43 -0400

    TODO change

--- TODO.js
+++ TODO.js
@@ -47,7 +47,7 @@
 // "Nunavut" X0A 0H0;
 // "Nortwest Territories" X1A 2R3;
 
-"election_info": {
-    "election_date": "November 7, 2022",
-    "election_website": "https://www.elections.gov.nl.ca/elections/"
-},
+// "election_info": {
+//     "election_date": "November 7, 2022",
+//     "election_website": "https://www.elections.gov.nl.ca/elections/"
+// },
